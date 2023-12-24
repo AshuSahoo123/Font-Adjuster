2df15a6a69184fbc9ed6c3440dfcadb1b11c6ecf
AshuSahoo123 <84637653+AshuSahoo123@users.noreply.github.com>
2023-12-23 23:32:40 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
     canvas = createCanvas(600,500);
     canvas.position(560, 150);
@@ -12,8 +18,24 @@ function modelLoaded() {
     console.log("Posenet is initialized!");
 }
 
+function draw() {
+    background("#87CEEB");
+    document.getElementById("font-size").innerHTML = "The font-size will be " + difference + " pixels.";
+    stroke('#8B0000');
+    text("Ashu", noseX, noseY);
+    textSize(difference);
+}
+
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + ", and noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + ", and rightWristX = " + rightWristX + ". Therefore, the difference is " + difference); 
     }
 }
