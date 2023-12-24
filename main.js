96f58761cdbb921733df3f5e94066e279acb2bec
AshuSahoo123 <84637653+AshuSahoo123@users.noreply.github.com>
2023-12-23 23:12:50 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,4 +3,17 @@ function setup() {
     canvas.position(560, 150);
     video = createCapture(VIDEO);
     video.size(550, 600);
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Posenet is initialized!");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
 }
